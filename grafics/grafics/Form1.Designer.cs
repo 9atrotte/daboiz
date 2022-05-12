3ef13a3f4c19aa3a606cd4b15334a47e9914b144
9atrotte <104942752+9atrotte@users.noreply.github.com>
2022-05-13 11:00:24 +1200

    Add files via upload

---
+++ grafics/grafics/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace grafics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic_paper = new System.Windows.Forms.PictureBox();
+            this.btn_draw = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_paper)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_paper
+            // 
+            this.pic_paper.Location = new System.Drawing.Point(12, 12);
+            this.pic_paper.Name = "pic_paper";
+            this.pic_paper.Size = new System.Drawing.Size(604, 426);
+            this.pic_paper.TabIndex = 0;
+            this.pic_paper.TabStop = false;
+            this.pic_paper.Click += new System.EventHandler(this.pic_paper_Click);
+            // 
+            // btn_draw
+            // 
+            this.btn_draw.Location = new System.Drawing.Point(666, 287);
+            this.btn_draw.Name = "btn_draw";
+            this.btn_draw.Size = new System.Drawing.Size(75, 23);
+            this.btn_draw.TabIndex = 1;
+            this.btn_draw.Text = "draw";
+            this.btn_draw.UseVisualStyleBackColor = true;
+            this.btn_draw.Click += new System.EventHandler(this.btn_draw_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(666, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(666, 345);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(666, 374);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_draw);
+            this.Controls.Add(this.pic_paper);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_paper)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_paper;
+        private System.Windows.Forms.Button btn_draw;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ grafics/grafics/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grafics
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            pic_paper.Refresh();
+
+        }
+
+        private void pic_paper_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_draw_Click(object sender, EventArgs e)
+        {
+            
+            Graphics paper;
+            paper = pic_paper.CreateGraphics();
+            Pen mypen = new Pen(Color.Black, 5);
+            paper.DrawLine(mypen, 10, 10, 50, 50);
+            this.Location =new Point(0,0);
+            var x = pic_paper.Location;
+            var y = MousePosition;
+            var z = this.Location;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(this);
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine(this);
+            
+        }
+    }
+}
